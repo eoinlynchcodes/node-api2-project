b3d692de4bb43bdc37c5ab0c79e12ee1593527dc
eoinlynchcodes <eoin95lynch@gmail.com>
2020-03-10 17:09:11 +0000

    For Nabeelah

--- data/apis/posts-router.js
+++ data/apis/posts-router.js
@@ -1,67 +1,82 @@
-const express = require('express')
+const express = require("express");
 
-const router = express.Router()
+const helperFunctions = require("../db");
 
-router.post('/api/posts', (req, res) => {
-    if(!req.body.title || !req.body.contents){
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
-    } else if( req.body.title === true || req.body.contents === true ) {
-        insert(post);
-        res.status(201);
-        return post;
-    } else {
-        // how to cancel the request???
-        res.status(500).json({ error: "There was an error while saving the post to the database"})
-    }
-})
+const router = express.Router();
 
-router.post('/api/posts/:id/comments', (req, res) => {
-    if(req.id === null){
-        res.status(404).json({ message: "The post with the specified ID does not exist."})
-    } else if( req.text === null) {
-        // How do you cancel the request?
-        res.status(400).json({ errorMessage: "There was an error while saving the post to the database."})
-    } else {
-        insertComment(comment)
-        res.status(201);
-        return res;    
-    }
-})
-
-// router.get('/api/posts', (req, res) => {
-
+// router.post('/api/posts', (req, res) => {
+//     if(!req.body.title || !req.body.contents){
+//         res.status(400).json({ errorMessage: "Please provide title and contents for the post."})
+//     } else if( req.body.title === true || req.body.contents === true ) {
+//         insert(post);
+//         res.status(201);
+//         return post;
+//     } else {
+//         // how to cancel the request???
+//         res.status(500).json({ error: "There was an error while saving the post to the database"})
+//     }
 // })
 
-router.get('/api/posts/id', (req, res) => {
-    if(req.id === null){
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    } if(req === null){
-        res.status(500).json({ message: "The post with the specified ID does not exist." })
-    }
-})
+router.get("/api/posts", (req, res) => {
+  helperFunctions
+    .find()
+    .then(res => {
+      res.status(200).json(res.data);
+    })
+    .catch(error => {
+      res
+        .status(500)
+        .json({ error: "The posts information could not be retrieved." });
+    });
+});
 
-router.get('/api/posts/:id/comments', (req, res) => {
-    if(req === null){
-        res.status(404).json({message: "The post with the specified ID does not exist."})
-    } 
-})
+// router.post('/api/posts/:id/comments', (req, res) => {
+//     if(req.id === null){
+//         res.status(404).json({ message: "The post with the specified ID does not exist."})
+//     } else if( req.text === null) {
+//         // How do you cancel the request?
+//         res.status(400).json({ errorMessage: "There was an error while saving the post to the database."})
+//     } else {
+//         insertComment(comment)
+//         res.status(201);
+//         return res;
+//     }
+// })
 
-router.delete('/api/posts/:id', (req, res) => {
-    if(req.id === null){
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    }
-})
+// // router.get('/api/posts', (req, res) => {
 
-router.put('/api/posts/:id', (req, res) => {
-    const { comment } = req.comment;
+// // })
 
-    if(req.id === null){
-        res.status(404).json({ message: "The post with the specified ID does not exist." })
-    } else if(req.body === null || req.contents === null){
-        res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
-    } else if( req === true){
+// router.get('/api/posts/id', (req, res) => {
+//     if(req.id === null){
+//         res.status(404).json({ message: "The post with the specified ID does not exist." })
+//     } if(req === null){
+//         res.status(500).json({ message: "The post with the specified ID does not exist." })
+//     }
+// })
 
-    }
-})
+// router.get('/api/posts/:id/comments', (req, res) => {
+//     if(req === null){
+//         res.status(404).json({message: "The post with the specified ID does not exist."})
+//     }
+// })
 
-module.exports = router
+// router.delete('/api/posts/:id', (req, res) => {
+//     if(req.id === null){
+//         res.status(404).json({ message: "The post with the specified ID does not exist." })
+//     }
+// })
+
+// router.put('/api/posts/:id', (req, res) => {
+//     const { comment } = req.comment;
+
+//     if(req.id === null){
+//         res.status(404).json({ message: "The post with the specified ID does not exist." })
+//     } else if(req.body === null || req.contents === null){
+//         res.status(400).json({ errorMessage: "Please provide title and contents for the post." })
+//     } else if( req === true){
+
+//     }
+// })
+
+module.exports = router;
